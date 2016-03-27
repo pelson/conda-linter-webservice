1985ea0a34fc3b43a77dae714c8d5d3d015077c7
Phil Elson <pelson.pub@gmail.com>
2016-03-27 18:42:53 +0100

    Tried using a different conda buildpack.

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello world")
 
+
+class FooHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello foo")
+
+
 def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/foo", FooHandler),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
     main()
